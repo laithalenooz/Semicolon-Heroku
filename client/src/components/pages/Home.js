00ce69bd005bb92e7138b6fad72badc932a8354c
laithalenooz <laithalenooz@gmail.com>
2021-02-19 18:33:30 +0200

    updated UI

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,7 @@
 import React, {Fragment, useContext, useEffect, useState} from 'react';
 import axios from "axios";
 import AuthContext from "../../context/auth/authContext";
-
-
+import { Segment, Image, Button, Comment, Form } from 'semantic-ui-react';
 
 const Home = () => {
     const authContext = useContext(AuthContext);
@@ -21,31 +20,24 @@ const Home = () => {
             setPosts(response.data);
         })
 
+
     return (
         <Fragment>
-                    {posts && posts.map(post => (
-                                <Fragment>
-                                    {post.type === 'public' ? <div key={post._id} className={'card bg-light'}>
-                                        <h3 className="text-primary text-left">
-                                            {post.title}{' '}
-                                            <span style={{float: 'right'}} className={'badge ' +
-                                            (post.type === 'public' ? 'badge-success' : 'badge-danger')}
-                                            >
-                                            {post.type.charAt(0).toUpperCase() + post.type.slice(1)}
-                                            </span>
-                                        </h3>
-                                        <h3>
-                                            <ul className="list">
-                                                {post.description && (<li>
-                                                    {post.description}
-                                                </li>)}
-                                            </ul>
-                                        </h3>
-                                    </div> : ''}
-
-                                </Fragment>))}
+              {posts && posts.map(post => (
+           <Fragment key={post._id}>
+               {post.type === 'public' ?
+               <Segment>
+                   <span className={'badge badge-success'}>{post.type}</span>
+                 <Image src={post.image} size='small' floated='left' />
+                 <h3>{post.title}</h3>
+                 <p>
+                    {post.description}
+                 </p>
+                <p className={'bg-dark'}>{post.date}</p>
+             </Segment> : ''}
+           </Fragment>))}
         </Fragment>
     );
-};
+}
 
 export default Home;
